c521ab7ef03715096e4d207d85576c3a04fe79a9
Gabriel <gab.matos@outlook.com>
2021-11-17 22:28:46 +0000

    First commit for displaying data

---
+++ src/Contacts.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function Contacts() {
+    const [message, setMessage] = useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:8080/moviesDB/movies').then(res=>{
+            return res.json()}).then(data=>{
+            const newData = Object.values(data)
+            setMessage(newData)
+
+        })
+            }, [])
+
+ return (
+ <main>
+        {message.map(e =>(
+            <>
+             <ul>
+             <li>{e.film_id}</li>
+             <li>{e.title}</li>
+             <li>{e.description}</li>
+             </ul>
+             </>
+        )
+        )}
+ </main>
+ );
+}
+
+export default Contacts;
