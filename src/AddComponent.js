24090e0eefad15af561dcf947dc2be52c9471ebc
Gabriel <gab.matos@outlook.com>
2021-11-25 11:25:45 +0000

    Added Add component

---
+++ src/AddComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class Table extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    async componentDidMount() {
+
+    }
+
+    render() {
+        const{films, isLoading, isError} = this.state
+    }
+}
+
+export default Table
